b4112c8ac5cb6298bf6c5b20b625e0102734468c
Shamuratov222 <d.schamuratov2014@yandex.ru>
2021-12-03 19:44:45 +0500

    Добавление ссылок на все страницы

--- WWW/index.php
+++ WWW/index.php
@@ -3,6 +3,12 @@
 </html>
 <p> Дата и время:
 <p>
+<ul>
+<li><a href="/denwer/PHP-Shamuratov/WWW/index.php">Главная</a></li>
+<li><a href="/denwer/PHP-Shamuratov/WWW/lab-1-1.php">Упражнение 1-2</a></li>
+<li><a href="/denwer/PHP-Shamuratov/WWW/lab-2-2.php">Упражнение 1-3</a></li>
+<li><a href="/denwer/PHP-Shamuratov/WWW/lab-3-3.php">Упражнение 1-4</a></li>
+</ul>
 <?php
 $d=date("d.m.Y H:i");
 echo($d);
