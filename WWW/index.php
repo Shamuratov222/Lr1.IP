f7e39542b18ec8b9ba384ae8281b9b8a2e86b569
Shamuratov222 <d.schamuratov2014@yandex.ru>
2021-12-03 20:10:32 +0500

    Самостоятельная работа 1 и добавление новой ссылки

--- WWW/index.php
+++ WWW/index.php
@@ -8,6 +8,7 @@
 <li><a href="/denwer/PHP-Shamuratov/WWW/lab-1-1.php">Упражнение 1-2</a></li>
 <li><a href="/denwer/PHP-Shamuratov/WWW/lab-2-2.php">Упражнение 1-3</a></li>
 <li><a href="/denwer/PHP-Shamuratov/WWW/lab-3-3.php">Упражнение 1-4</a></li>
+<li><a href="/denwer/PHP-Shamuratov/WWW/lab-1-5.php">Самостоятельная работа 1</a></li>
 </ul>
 <?php
 $d=date("d.m.Y H:i");
---
+++ WWW/lab-1-5.php
@@ -0,0 +1,13 @@
+<html>
+<head><title>Shamuratov Damir</title></head>
+</html>
+<p> Variant 8
+<p>
+<?php
+$a=rand(-20,20);
+$b=rand(-20,20);
+$c=rand(-20,20);
+$d=rand(-20,20);
+$res=(($b*(2*$c)+$d-52)/($a/3)+1);
+echo('('.$b.'2*'.$c.'+'.$d.'-52)/'.'('.$a.'/'.'3)'.'+1='.$res); 
+?>
